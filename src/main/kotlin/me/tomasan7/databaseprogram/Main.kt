07ab89ae8a2b58c6469909e3dcd2ba8ea47779fc
Tomasan7 <tomashula06@gmail.com>
2024-03-03 17:32:24 +0100

    Update Main

--- src/main/kotlin/me/tomasan7/databaseprogram/Main.kt
+++ src/main/kotlin/me/tomasan7/databaseprogram/Main.kt
@@ -1,26 +1,40 @@
 package me.tomasan7.databaseprogram
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import me.tomasan7.databaseprogram.registerpage.RegisterPage
+import me.tomasan7.databaseprogram.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App()
 {
-    var name by remember { mutableStateOf("") }
-
-    Row {
-        TextField(name, onValueChange = { name = it }, label = { Text("Username") })
-    }
+    RegisterPage(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    )
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    AppTheme {
+        Window(onCloseRequest = ::exitApplication) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                App()
+            }
+        }
     }
 }
