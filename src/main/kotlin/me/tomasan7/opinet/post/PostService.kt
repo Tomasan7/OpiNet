c6783b3b084cedf1bc9558b2f883cb17fde2d0a9
Tomasan7 <tomashula06@gmail.com>
2024-03-08 19:45:32 +0100

    Document PostService and UserService

--- src/main/kotlin/me/tomasan7/opinet/post/PostService.kt
+++ src/main/kotlin/me/tomasan7/opinet/post/PostService.kt
@@ -2,12 +2,28 @@ package me.tomasan7.opinet.post
 
 import kotlinx.collections.immutable.ImmutableList
 
+/** Manages posts. */
 interface PostService
 {
+    /**
+     * Creates a new post.
+     *
+     * @return The post's new id.
+     */
     suspend fun createPost(postDto: PostDto): Int
+
+    /** Returns a [post][PostDto] with [id]. Or `null` if not found. */
     suspend fun getPostById(id: Int): PostDto?
+
+    /** Returns all posts ordered by upload date descending. */
     suspend fun getAllPostsOrderedByUploadDateDesc(): ImmutableList<PostDto>
+
+    /** Returns all posts by user with id [authorId] ordered by upload date descending. */
     suspend fun getPostsByAuthorIdOrderedByUploadDateDesc(authorId: Int): ImmutableList<PostDto>
+
+    /** Updates a post using the id in [postDto]. */
     suspend fun updatePost(postDto: PostDto): Boolean
+
+    /** Deletes a post with id [id]. */
     suspend fun deletePost(id: Int): Boolean
 }
--- src/main/kotlin/me/tomasan7/opinet/user/UserService.kt
+++ src/main/kotlin/me/tomasan7/opinet/user/UserService.kt
@@ -1,11 +1,23 @@
 package me.tomasan7.opinet.user
 
+/** Manages users. */
 interface UserService
 {
-    /** @throws UsernameAlreadyExistsException */
+    /**
+     * Creates a new user with [password].
+     *
+     * @return The user's new id.
+     * @throws UsernameAlreadyExistsException
+     */
     suspend fun createUser(userDto: UserDto, password: String): Int
+
+    /** Returns a [user][UserDto] with [id]. Or `null` if not found. */
     suspend fun getUserById(id: Int): UserDto?
+
+    /** Returns a [user][UserDto] with [username]. Or `null` if not found. */
     suspend fun getUserByUsername(username: String): UserDto?
+
+    /** Returns `true` if the user with [username] exists and his password matches with [password]. */
     suspend fun loginUser(username: String, password: String): Boolean
 }
 
