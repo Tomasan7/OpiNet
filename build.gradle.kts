1ca8ab25054d64a144b6e66630ae46dc67ec1fe4
Tomasan7 <tomashula06@gmail.com>
2024-03-08 16:14:25 +0100

    Change distributions package name

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "opinet"
+            packageName = "OpiNet"
             packageVersion = "1.0.0"
         }
     }
