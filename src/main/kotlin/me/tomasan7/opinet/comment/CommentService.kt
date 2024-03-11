6064675edf01b6a71d7fdbf8f03236deee9ff818
Tomasan7 <tomashula06@gmail.com>
2024-03-11 10:59:12 +0100

    Add KDoc to service functions

--- src/main/kotlin/me/tomasan7/opinet/comment/CommentService.kt
+++ src/main/kotlin/me/tomasan7/opinet/comment/CommentService.kt
@@ -4,10 +4,29 @@ import kotlinx.collections.immutable.ImmutableList
 
 interface CommentService
 {
+    /**
+     * Creates a comment for a post.
+     *
+     * @return The id of the new comment.
+     */
     suspend fun createComment(comment: CommentDto): Int
+
+    /** Returns a comment by its [id] or `null` if this id doesn't exist. */
     suspend fun getCommentById(id: Int): CommentDto?
+
+    /** Returns all comments on a [post][postId]. May be empty. */
     suspend fun getAllCommentsForPostOrderedByUploadDateDesc(postId: Int): ImmutableList<CommentDto>
+
+    /** Returns all comments by a [user][authorId]. May be empty. */
     suspend fun getAllCommentsForUser(authorId: Int): ImmutableList<CommentDto>
+
+    /** Returns the number of comments on a [post][postId]. */
     suspend fun getNumberOfCommentsForPost(postId: Int): Long
+
+    /**
+     * Deletes a comment by its [id][postId].
+     *
+     * @return The number of comments deleted.
+     */
     suspend fun deleteCommentsForPost(postId: Int): Int
 }
--- src/main/kotlin/me/tomasan7/opinet/votes/VotesService.kt
+++ src/main/kotlin/me/tomasan7/opinet/votes/VotesService.kt
@@ -4,9 +4,22 @@ import kotlinx.collections.immutable.ImmutableList
 
 interface VotesService
 {
+    /**
+     * Creates a vote for a post.
+     *
+     * @return The id of the new vote.
+     */
     suspend fun createVote(voteDto: VoteDto): Int
+
+    /** Gets the votes on a post. */
     suspend fun getVotesOnPost(postId: Int): ImmutableList<VoteDto>
+
+    /** Gets ids of posts with most votes overall. */
     suspend fun getPostsOrderedByVotes(): ImmutableList<Int>
+
+    /** Removes a vote by a user on a post. */
     suspend fun removeVoteByUserOnPost(userId: Int, postId: Int): Boolean
+
+    /** Deletes all votes for a post. */
     suspend fun deleteVotesForPost(postId: Int)
 }
